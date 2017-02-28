45c25c27921e9e2e669535774e4568f57ea8c617
Ilja Sillman <isillman@gmail.com>
2017-02-28 21:06:38 +0200

    tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync').create();
 
 
-
 // Source files
 // ****************************************************************************
 var htmlSrc = './app/*.html';
@@ -18,7 +17,7 @@ var scssSrc = './app/scss/**/*.scss';
 var cssTgt = './app/css/'; // Compiled css + sourcemaps go here
 
 
-// Static Server + watching scss/html files
+// Static Server + watch
 gulp.task('serve', ['sass'], function () {
     browserSync.init({
         server: './app'
@@ -27,7 +26,7 @@ gulp.task('serve', ['sass'], function () {
     gulp.watch(htmlSrc).on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
+// Compile sass & refresh browsers
 gulp.task('sass', function () {
     return gulp.src(scssSrc)
         .pipe(sourcemaps.init())
